64b5297c456b4bd81ca50d202f98e35729aeb949
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2022-03-28 19:08:01 +0200

    added bigger/smaller paddle, ordered the levels correctly

---
+++ Assets/bonus/PaddleBigBonus.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBigBonus : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PaddleControl p = collision.gameObject.GetComponent<PaddleControl>();
+        if (p != null)
+        {
+            Vector3 scale = collision.transform.localScale;
+            collision.transform.localScale = new Vector3(scale.x + .2f, 1f, 1f);
+        }
+    }
+}
